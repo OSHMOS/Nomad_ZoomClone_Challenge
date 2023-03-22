301129a6d173729b989ca9f29f48362d0df2754c
oshmos <ojames9707@gmail.com>
2023-03-22 19:02:22 +0900

    노마드 줌클론 챌린지 채팅 완성하기 콘솔 버전 : 03.22

--- 3월 20일, 21일/src/public/js/app.js
+++ 3월 20일, 21일/src/public/js/app.js
@@ -1,4 +1,7 @@
-alert('hi'); // frontend에서 backend랑 연결해 달라고 해야 앞에서 만든 wss.on의 connection이 작동한다!
+// app.js
+
+const messageList = document.querySelector('ul');
+const messageForm = document.querySelector('form');
 
 const socket = new WebSocket(`ws://${window.location.host}`); // 이제 서버로 접속 가능! - 여기 socket을 이용해서 frontend에서 backend로 메세지 전송 가능!
 // 여기 socket은 서버로의 연결
@@ -15,6 +18,15 @@ socket.addEventListener("close", () => {
   console.log("Disconnected to Server ❌");
 });
 
-setTimeout(() => {
-  socket.send("hello from the browser!"); // backend로 메시지 보내기!
-}, 10000); // 10초 후에 작동
+// setTimeout(() => {
+//   socket.send("hello from the browser!"); // backend로 메시지 보내기!
+// }, 10000); // 10초 후에 작동
+
+function handleSubmit(event){
+  event.preventDefault();
+  const input = messageForm.querySelector('input');
+  socket.send(input.value);
+  input.value = '';
+}
+
+messageForm.addEventListener('submit', handleSubmit);
--- 3월 20일, 21일/src/server.js
+++ 3월 20일, 21일/src/server.js
@@ -1,6 +1,7 @@
 import http from "http";
 import WebSocket from "ws"; // npm i ws
 import express from "express";
+import { SocketAddress } from "net";
 
 const app = express();
 
@@ -17,18 +18,22 @@ const server = http.createServer(app); // app은 requestlistener 경로 - expres
 
 const wss = new WebSocket.Server({ server }); // http 서버 위에 webSocket서버 생성, 위의 http로 만든 server는 필수 X - 이렇게 하면 http / ws 서버 모두 같은 3000번 포트를 이용해서 돌릴 수 있다!
 
+
+const sockets = []; // 누군가 우리 채팅방에 접속하면 connection을 해당 배열에 넣을 생각
+
 // on method에서는 event가 발동되는 것을 기다린다
 // event가 connection / 뒤에 오는 함수는 event가 일어나면 작동
 // 그리고 on method는 backend에 연결된 사람의 정보를 제공 - 그게 socket에서 옴
 // 익명함수로 바꾸기
 wss.on("connection", socket => { // 여기의 socket이라는 매개변수는 새로운 브라우저를 뜻함!! (wss는 전체 서버, socket은 하나의 연결이라고 생각!!)
+  sockets.push(socket);
   console.log("Connected to Browser ✅");
   socket.on("close", () => console.log("Disconnected to Server ❌")); // 서버를 끄면 동작
   socket.on("message", message => {
       const utf8message = message.toString("utf8"); // 버퍼 형태로 전달되기 때문에 toString 메서드를 이용해서 utf8로 변환 필요!
-      console.log(utf8message);
+      sockets.forEach(aSocket => aSocket.send(utf8message)); // 연결된 모든 소켓에 해당 메시지 전달!!
+      // socket.send(utf8message);
   }); // 프론트엔드로부터 메시지가 오면 콘솔에 출력
-  socket.send("hello!!!"); // hello 메시지 보내기 - send는 socket의 전송용 메서드!!
 }) // socket을 callback으로 받는다! webSocket은 서버와 브라우저 사이의 연결!!!
 
 server.listen(3000, handleListen); // 서버는 ws, http 프로토콜 모두 이해할 수 있게 된다!
