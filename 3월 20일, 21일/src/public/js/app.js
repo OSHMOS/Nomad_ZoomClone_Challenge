77eb7a6b67f3b6e499356703eb217d099f14c5c7
oshmos <ojames9707@gmail.com>
2023-03-22 12:09:17 +0900

    노마드 줌클론 챌린지 웹소켓의 기본:03.22

--- 3월 20일, 21일/src/public/js/app.js
+++ 3월 20일, 21일/src/public/js/app.js
@@ -1,1 +1,4 @@
-alert('hi');
+alert('hi'); // frontend에서 backend랑 연결해 달라고 해야 앞에서 만든 wss.on의 connection이 작동한다!
+
+const socket = new WebSocket(`ws://${window.location.host}`); // 이제 서버로 접속 가능! - 여기 socket을 이용해서 frontend에서 backend로 메세지 전송 가능!
+// 여기 socket은 서버로의 연결
--- 3월 20일, 21일/src/server.js
+++ 3월 20일, 21일/src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws"; // npm i ws
 import express from "express";
 
 const app = express();
@@ -9,4 +11,21 @@ app.get("/", (req, res) => res.render("home")); // 홈페이지로 이동할 때
 app.get("/*", (req, res) => res.redirect("/")) // 홈페이지 내 어느 페이지에 접근해도 홈으로 연결되도록 리다이렉트 (다른 url 사용 안할거라)
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`)
-app.listen(3000, handleListen);
+// app.listen(3000, handleListen);
+
+const server = http.createServer(app); // app은 requestlistener 경로 - express application으로부터 서버 생성
+
+const wss = new WebSocket.Server({ server }); // http 서버 위에 webSocket서버 생성, 위의 http로 만든 server는 필수 X - 이렇게 하면 http / ws 서버 모두 같은 3000번 포트를 이용해서 돌릴 수 있다!
+
+// 임시로 만든 함수
+function handleConnection(socket) { // 여기서 socket은 연결된 브라우저
+    console.log(socket) // 여기 있는 소켓이 frontend와 real-time으로 소통할 수 있다!
+};
+
+// on method에서는 event가 발동되는 것을 기다린다
+// event가 connection / 뒤에 오는 함수는 event가 일어나면 작동
+// 그리고 on method는 backend에 연결된 사람의 정보를 제공 - 그게 socket에서 옴
+wss.on("connection", handleConnection) // socket을 callback으로 받는다! webSocket은 서버와 브라우저 사이의 연결!!!
+
+
+server.listen(3000, handleListen); // 서버는 ws, http 프로토콜 모두 이해할 수 있게 된다!
