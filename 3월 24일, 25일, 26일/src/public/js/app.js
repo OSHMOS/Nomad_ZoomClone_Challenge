1393324a17356f543863616d55f3e27a4b6b007b
oshmos <ojames9707@gmail.com>
2023-03-26 21:58:11 +0900

    노마드 줌클론 챌린지 음소거, 카메라 끄기/켜기  버튼 생성 및 기능 구현

--- 3월 24일, 25일, 26일/src/public/js/app.js
+++ 3월 24일, 25일, 26일/src/public/js/app.js
@@ -3,9 +3,41 @@
 const socket = io(); // io function은 알아서 socket.io를 실행하고 있는 서버를 찾을 것이다!
 
 const myFace = document.getElementById("myFace");
+const muteBtn = document.getElementById("mute");
+const cameraBtn = document.getElementById("camera");
 
 let myStream;
 // stream 받기 : stream은 비디오와 오디오가 결합된 것
+
+let muted = false; // 처음에는 음성을 받음
+let cameraOff = false; // 처음에는 영상을 받음
+
+function handleMuteClick() {
+  myStream.getAudioTracks().forEach(tracks => tracks.enabled = !tracks.enabled);
+  if(!muted) {
+    muteBtn.innerText = "Unmute";
+    muted = true;
+  }
+  else {
+    muteBtn.innerText = "Mute";
+    muted = false;
+  }
+}
+
+function handleCameraClick() {
+  myStream.getVideoTracks().forEach(tracks => tracks.enabled = !tracks.enabled);
+  if(cameraOff) {
+    cameraBtn.innerText = "Turn Camera Off";
+    cameraOff = false;
+  }
+  else {
+    cameraBtn.innerText = "Turn Camera On";
+    cameraOff = true;
+  }
+}
+
+muteBtn.addEventListener("click", handleMuteClick);
+cameraBtn.addEventListener("click", handleCameraClick);
 
 // https://developer.mozilla.org/ko/docs/Web/API/MediaDevices/getUserMedia 사용 : 유저의 유저미디어 string을 받기위함
 async function getMedia() {
